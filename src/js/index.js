96b129990547417f6454f0cd3a08aa5b04ebd1c4
bashh69 <bashh@hotmail.com>
2022-04-16 17:31:27 +0000

    firstest commit

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,13 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import { Counter } from "./component/Counter.js";
+let secs = 1;
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+const timer = setInterval(() => {
+	if (secs === 999999999) {
+		clearInterval(timer);
+	}
+	ReactDOM.render(<Counter seconds={secs} />, document.querySelector("#app"));
+	secs++;
+}, 1000);
